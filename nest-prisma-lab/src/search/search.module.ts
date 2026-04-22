406ee0626ad35b5025f38eff46ed2ff127624e69
pearlispearl <rawisarachantra@gmail.com>
2026-04-22 21:29:53 +0700

    feat: add cachemodule

--- nest-prisma-lab/src/search/search.module.ts
+++ nest-prisma-lab/src/search/search.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { SearchService } from './search.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { SearchController } from './search.controller';
 
 @Module({
+  imports: [CacheModule.register()],   
   controllers: [SearchController],
   providers: [SearchService, PrismaService]
 })
