1d0b6d84d80f925d2ce0ca52f7aef0f64d06c538
JarukitMan <pleaseletmeinijustwanttoemail@gmail.com>
2026-04-22 11:51:47 +0700

    feat: deferred some logic to prisma 	modified:   nest-prisma-lab/src/search/search.service.ts

--- nest-prisma-lab/src/search/search.service.ts
+++ nest-prisma-lab/src/search/search.service.ts
@@ -7,24 +7,24 @@ export class SearchService {
   constructor(private readonly prisma: PrismaService) {}
 
   async search(dto: SearchQueryDto) {
-    const all_rooms = await this.prisma.rooms.findMany()
-    return all_rooms.filter(
+    const rooms = await this.prisma.rooms.findMany(
+      {
+        where: {
+          AND: [
+            { capacity: {gte: dto.min_capacity} },
+            { capacity: {lte: dto.max_capacity} },
+            { price_per_night: {gte: dto.max_price_per_night} },
+            { price_per_night: {lte: dto.min_price_per_night} },
+          ]
+        }
+      }
+    )
+    return rooms.filter(
       room => {
         if (room.name && dto.name &&
           !room.name.includes(dto.name)) return false
         if (room.description && dto.description &&
           !room.description.includes(dto.description)) return false
-
-        if (room.capacity && dto.min_capacity &&
-          room.capacity < dto.min_capacity) return false
-        if (room.capacity && dto.max_capacity &&
-          room.capacity > dto.max_capacity) return false
-
-        if (room.price_per_night && dto.min_price_per_night &&
-          room.price_per_night < dto.min_price_per_night) return false
-        if (room.price_per_night && dto.max_price_per_night &&
-          room.price_per_night > dto.max_price_per_night) return false
-        // TODO: Might need to add date range.
 
         return room.is_active
       }
