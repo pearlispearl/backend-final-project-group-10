ac4bd1f89d09903ef873770712041b6ea9d03433
pearlispearl <rawisarachantra@gmail.com>
2026-04-22 21:28:39 +0700

    add SearchQueryDto

--- nest-prisma-lab/src/search/search.controller.ts
+++ nest-prisma-lab/src/search/search.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 import { SearchService } from './search.service';
+import { SearchQueryDto } from './dto/search-query.dto';
 import { CacheInterceptor } from '@nestjs/cache-manager';
-import {  ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Decimal } from '@prisma/client/runtime/client';
 
 @Controller('search')
@@ -9,9 +10,9 @@ import { Decimal } from '@prisma/client/runtime/client';
 @ApiTags('search')
 // Conscious decision not to require login. I'd be unwilling to use a service that requires me to log in to search.
 export class SearchController {
-  constructor(private readonly search: SearchService) {}
+  constructor(private readonly search: SearchService) { }
 
-  @ApiOperation({summary: "Retrieve all rooms that pass the search query"})
+  @ApiOperation({ summary: "Retrieve all rooms that pass the search query" })
   @ApiResponse({
     status: 200,
     description: 'List of all rooms that fit criteria',
@@ -41,26 +42,17 @@ export class SearchController {
       },
     },
   })
-  @ApiQuery({ name: 'name', required: false, type: String, description: 'Partial or full name of the room', example: 'simple'})
-  @ApiQuery({ name: 'description', required: false, type: String, description: 'Partial or full description of the room', example: 'amenities'})
-  @ApiQuery({ name: 'min_capacity', required: false, type: Number, description: 'Minimum acceptable capacity the room should have', example: 1})
-  @ApiQuery({ name: 'max_capacity', required: false, type: Number, description: 'Maximum acceptable capacity the room should have', example: 10})
-  @ApiQuery({ name: 'min_price_per_night', required: false, type: Decimal, description: 'Minimum acceptable price of the room', example: Decimal(1000)})
-  @ApiQuery({ name: 'max_price_per_night', required: false, type: Decimal, description: 'Maximum acceptable price of the room', example: Decimal(10000)})
-  @ApiQuery({ name: 'start_date', required: true, type: Date, description: 'Start of the date range the room needs to be available', example: '2027-12-24T00:00:00.000Z'})
-  @ApiQuery({ name: 'end_date', required: true, type: Date, description: 'End of the date range the room needs to be available', example: '2028-01-01T08:00:00.000Z'})
+  @ApiQuery({ name: 'name', required: false, type: String, description: 'Partial or full name of the room', example: 'simple' })
+  @ApiQuery({ name: 'description', required: false, type: String, description: 'Partial or full description of the room', example: 'amenities' })
+  @ApiQuery({ name: 'min_capacity', required: false, type: Number, description: 'Minimum acceptable capacity the room should have', example: 1 })
+  @ApiQuery({ name: 'max_capacity', required: false, type: Number, description: 'Maximum acceptable capacity the room should have', example: 10 })
+  @ApiQuery({ name: 'min_price_per_night', required: false, type: Decimal, description: 'Minimum acceptable price of the room', example: Decimal(1000) })
+  @ApiQuery({ name: 'max_price_per_night', required: false, type: Decimal, description: 'Maximum acceptable price of the room', example: Decimal(10000) })
+  @ApiQuery({ name: 'start_date', required: true, type: Date, description: 'Start of the date range the room needs to be available', example: '2027-12-24T00:00:00.000Z' })
+  @ApiQuery({ name: 'end_date', required: true, type: Date, description: 'End of the date range the room needs to be available', example: '2028-01-01T08:00:00.000Z' })
   @ApiResponse({ status: 429, description: 'Too Many Requests' })
   @Get()
-  async get(@Query('name') name: string,
-    @Query('description') description: string,
-    @Query('min_capacity') min_capacity: number,
-    @Query('max_capacity') max_capacity: number,
-    @Query('min_price_per_night') min_price_per_night: Decimal,
-    @Query('max_price_per_night') max_price_per_night: Decimal,
-    @Query('start_date') start_date: Date,
-    @Query('end_date') end_date: Date,
-  ) {
-    const dto = {name, description, min_capacity, max_capacity, min_price_per_night, max_price_per_night, start_date, end_date}
-    return await this.search.search(dto)
+  async get(@Query() dto: SearchQueryDto) {   
+    return await this.search.search(dto);
   }
 }
